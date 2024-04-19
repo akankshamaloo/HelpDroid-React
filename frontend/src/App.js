bb9197352b91c4e1c975b3f65242402445c61b73
Riya Das <riya.das.it24@heritageit.edu.in>
2024-04-19 14:46:30 +0530

    minor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
 import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -10,22 +11,28 @@ import TimesheetList from "./pages/TimesheetList";
 import Home from "./pages/Home";
 import User_profile from "./pages/user_profile";
 import Patient from "./pages/patient";
+import PrescriptionUploadPage from "./pages/uploadPrescription";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/scorecard" element={<Scorecard />} />
-        <Route path="/forgotpassword" element={<Forgotpassword />} />
-        <Route path="/timesheetlist" element={<TimesheetList />} />
-        <Route path="/patient" element={<Patient />} />
-
-        <Route path="/userprofile" element={<User_profile />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Login />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="register" element={<Register />} />
+          <Route path="forgotpassword" element={<Forgotpassword />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="home" element={<Home />} />
+          <Route path="scorecard" element={<Scorecard />} />
+          <Route path="timesheetlist" element={<TimesheetList />} />
+          <Route path="patient" element={<Patient />} />
+          <Route
+            path="uploadprescription"
+            element={<PrescriptionUploadPage />}
+          />
+          <Route path="userprofile" element={<User_profile />} />
+        </Route>
       </Routes>
     </Router>
   );
