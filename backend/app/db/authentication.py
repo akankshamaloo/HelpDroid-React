67dfe04f7909df959fe3301478f19b6f1ede25af
Riya Das <riya.das.it24@heritageit.edu.in>
2024-04-23 05:44:15 +0530

    minor

--- backend/app/db/authentication.py
+++ backend/app/db/authentication.py
@@ -1,8 +1,8 @@
 from app.db.mongo import *
 from app.crypto.sha256 import *
 
-def reg_auth(email_input, password_input, name_input, mobile_input, role,dob,gender,category):
-    inserted_id = insert_data(email_input, password_input, mobile_input, name_input, role,dob,gender,category)
+def reg_auth(email_input, password_input, name_input, mobile_input, role,dob,gender,category,specialization,fees,experience):
+    inserted_id = insert_data(email_input, password_input, mobile_input, name_input, role,dob,gender,category,specialization,fees,experience)
     print(inserted_id)
     if inserted_id:
         return True, inserted_id  # Return the ObjectId as the second part of the tuple
